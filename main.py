ab6fa815f688997efc703ef76b1214535b4bfc98
yingmeng-01 <2680494019@qq.com>
2026-07-30 12:54:41 +0800

    示例 Python 文件

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+
+def greet(name):
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    print(greet("World"))
+pri
+os.uu
+import os
+
+os.abort
+
+
+def test(x, y):
+    return x + 
